dae80942fbad82038f30eb0851e379637b3d6c64
moffy-Black <black.river.emperor@outlook.jp>
2022-10-17 17:28:12 +0900

    [修正] index&App

--- src/app/src/App.tsx
+++ src/app/src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./pages/AuthContext";
 import Home from "./pages/Home";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
+import SignUp from "./pages/userAuth/SignUp";
+import SignIn from "./pages/userAuth/SignIn";
 import MyPage from "./pages/MyPage";
 import "./App.css";
 import MeetingRoom from "./pages/MeetingRoom";
@@ -12,12 +12,12 @@ const App = () => {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path={`/`} element={<Home />} />
-          <Route path={`/signup/`} element={<SignUp />} />
-          <Route path={`/signin/`} element={<SignIn />} />
-          <Route path={`/mypage/:content`} element={<MyPage />} />
+          <Route path={'/'} element={<Home />} />
+          <Route path={'/signup/'} element={<SignUp />} />
+          <Route path={'/signin/'} element={<SignIn />} />
+          <Route path={'/mypage/:content'} element={<MyPage />} />
           <Route
-            path={`/meeting-room/:token`}
+            path={'/meeting-room/:token'}
             element={<MeetingRoom />}
           ></Route>
         </Routes>
